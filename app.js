7f3ca050203495aca386ae1a39ae95f051497bfc
Max Grosshandler <maxgrosshandler@gmail.com>
2018-12-12 08:33:40 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+mongoose = require("mongoose"),
+bodyParser = require('body-parser')
+var app = express();
+
+var db = mongoose.connect("mongodb://localhost/restFolder", {
+    useNewUrlParser: true
+})
+
+var Book = require("./models/bookModel")
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+bookRouter = require('./Routes/bookRoutes')(Book)
+
+
+app.use("/api/books", bookRouter)
+
+app.get('/', function(req, res){
+    res.send("Welcome to my API!")
+})
+
+app.listen(port, function(){
+    console.log("Running on PORT: "+port)
+})
